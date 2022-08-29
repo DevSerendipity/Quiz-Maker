bb1cb6df0ff428bf3d6c85b0a55f440ff786e108
FoolProof <islamsakrak2002@gmail.com>
2022-08-29 08:00:47 +0200

    Improving the method name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         QuizMaker quizMaker = new QuizMaker();
-        quizMaker.quiz();
+        quizMaker.initializeQuiz();
     }
 }
